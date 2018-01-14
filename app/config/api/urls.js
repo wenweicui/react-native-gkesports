0d6ada31246f9f9387a7645f9527f5fee55f521a
铉嘉伟 <mifind@sina.com>
2018-01-14 20:52:18 +0800

    配置Android

--- app/config/api/urls.js
+++ app/config/api/urls.js
@@ -1,6 +1,6 @@
 const Urls = {
     Picture:{
-        getIdList:'/api/onelist/idlist/'
+        getIdList:'/api/onelist/idlist/?channel=wdj&version=4.0.2&uuid=ffffffff-a90e-706a-63f7-ccf973aae5ee&platform=android 或 http://v3.wufazhuce.com:8000/api/onelist/idlist/?channel=wdj&version=4.0.2&uuid=ffffffff-a90e-706a-63f7-ccf973aae5ee&platform=android'
     }
 };
 export default Urls
